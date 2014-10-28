0486c5b58cf5b7a3395226c7f5bc0902ba5cc17e
Andrew Phillips <andrew.phillips@ssc-spc.gc.ca>
2014-10-28 12:26:15 -0300

    Replace settingsDefault and buildUi with tables

--- crappy-plugins/alttab.lua
+++ crappy-plugins/alttab.lua
@@ -11,26 +11,32 @@ local plugin = {
          class = "global",
          description = "Integrate familiar Alt-Tab functionality in Awesome WM",
       },
+   },
+   options = {
+      {
+         name = 'modifier',
+         label = '_Modifier key name:',
+         type = 'string'
+      },
+      {
+         name = 'next',
+         label = '_Next window key name:',
+         type = 'string'
+      },
+      {
+         name = 'prev',
+         label = '_Previous window key name:',
+         type = 'string'
+      }
+   },
+   defaults = {
+      modifier = "Alt_L",
+      next = "Tab",
+      prev = "ISO_Left_Tab"
    }
 }
 
 local log = lgi.log.domain(plugin.id)
-
-function plugin.settingsDefault(settings)
-   if settings.modifier == nil then
-      settings.modifier = "Alt_L"
-   end
-
-   if settings.next == nil then
-      settings.next = "Tab"
-   end
-
-   if settings.prev == nil then
-      settings.prev = "ISO_Left_Tab"
-   end
-
-   return settings
-end
 
 function plugin.startup(awesomever, settings)
    alttab = require('alttab')
@@ -38,93 +44,6 @@ function plugin.startup(awesomever, settings)
    plugin.functions["alttab.start"].func = function()
       alttab(1, settings.modifier, settings.next, settings.prev)
    end
-end
-
-function plugin.buildUi(window, settings)
-   local Gtk = lgi.require('Gtk')
-
-   local modifierEntry = Gtk.Entry {
-      hexpand = true,
-   }
-
-   modifierEntry:set_text(settings.modifier)
-
-   function modifierEntry:on_changed()
-      settings.modifier = modifierEntry:get_text()
-   end
-
-   local nextEntry = Gtk.Entry {
-      hexpand = true,
-   }
-
-   nextEntry:set_text(settings.next)
-
-   function nextEntry:on_changed()
-      settings.next = nextEntry:get_text()
-   end
-
-   local prevEntry = Gtk.Entry {
-      hexpand = true,
-   }
-
-   prevEntry:set_text(settings.prev)
-
-   function prevEntry:on_changed()
-      settings.prev = prevEntry:get_text()
-   end
-
-   local row = -1;
-   local function nextRow()
-      row = row + 1
-      return row
-   end
-
-   return Gtk.Grid {
-      row_spacing = 6,
-      column_spacing = 6,
-      margin = 6,
-      expand = true,
-
-      {
-         left_attach = 0, top_attach = nextRow(),
-         Gtk.Label {
-            label = '_Modifier key name:',
-            halign = 'END',
-            use_underline = true,
-            mnemonic_widget = modifierEntry
-         },
-      },
-      {
-         left_attach = 1, top_attach = row,
-         modifierEntry
-      },
-      {
-         left_attach = 0, top_attach = nextRow(),
-         Gtk.Label {
-            label = '_Next window key name:',
-            halign = 'END',
-            use_underline = true,
-            mnemonic_widget = nextEntry
-         },
-      },
-      {
-         left_attach = 1, top_attach = row,
-         nextEntry
-      },
-      {
-         left_attach = 0, top_attach = nextRow(),
-         Gtk.Label {
-            label = '_Previous window key name:',
-            halign = 'END',
-            use_underline = true,
-            mnemonic_widget = prevEntry
-         },
-      },
-      {
-         left_attach = 1, top_attach = row,
-         prevEntry
-      },
-   }
 end
 
 return plugin
